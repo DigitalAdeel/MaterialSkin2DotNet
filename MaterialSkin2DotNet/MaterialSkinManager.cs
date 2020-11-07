e372173ec922cba5faf035a948aeea8043503804
Muhammad Adeel Abid <adeelabid234@outlook.com>
2020-11-07 08:54:54 +0500

    Default Primary Color is now Green and Accent Color is Blue.

--- MaterialSkin2DotNet/MaterialSkinManager.cs
+++ MaterialSkin2DotNet/MaterialSkinManager.cs
@@ -33,7 +33,7 @@
         // Constructor
         private MaterialSkinManager() {
             Theme = Themes.LIGHT;
-            ColorScheme = new ColorScheme(Primary.Indigo500, Primary.Indigo700, Primary.Indigo100, Accent.Pink200, TextShade.WHITE);
+            ColorScheme = new ColorScheme(Primary.Green500, Primary.Green700, Primary.Green100, Accent.Blue200, TextShade.WHITE);
 
             // Create and cache Roboto fonts
             // Thanks https://www.codeproject.com/Articles/42041/How-to-Use-a-Font-Without-Installing-it
